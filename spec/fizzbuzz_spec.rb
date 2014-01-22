53045f897c9a382e89b1050b3552c170d2326075
Ayazg <ayaz0206@gmail.com>
2014-01-22 09:37:03 +0000

    First test - divisible by three

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require "./fizzbuzz"
+
+describe "fizzbuzz" do
+
+context "In order to play, it" do
+	
+	it "tells me that 3 is divisible by 3" do
+		expect(is_divisible_by_three(3)).to be_true
+	end
+
+
+end
+end
